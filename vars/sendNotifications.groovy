d6ae3f57b8127403608cfe60b088ec724ada57a1
Victor Nahuel Chaves <nahuel.chaves@gmail.com>
2017-04-27 23:11:51 -0300

    Update sendNotifications.groovy

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -30,10 +30,10 @@ def call(String buildStatus = 'STARTED') {
   // Send notifications
   slackSend (color: colorCode, message: summary)
 
-  hipchatSend (color: color, notify: true, message: summary)
+  // hipchatSend (color: color, notify: true, message: summary)
 
   emailext (
-      to: 'bitwiseman@bitwiseman.com',
+      // to: 'bitwiseman@bitwiseman.com',
       subject: subject,
       body: details,
       recipientProviders: [[$class: 'DevelopersRecipientProvider']]
